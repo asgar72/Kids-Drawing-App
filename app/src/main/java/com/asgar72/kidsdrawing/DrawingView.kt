bdc8ebf867ff4c38246e94aeb6b3dcb6a99c2e84
asgar72 <72asgar72@gmail.com>
2024-02-08 23:05:02 +0530

    added

--- app/src/main/java/com/asgar72/kidsdrawing/DrawingView.kt
+++ app/src/main/java/com/asgar72/kidsdrawing/DrawingView.kt
@@ -7,93 +7,141 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
+import java.lang.reflect.TypeVariable
 import kotlin.io.path.Path
 
 class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
-    private var mDrawPath: CustomPath? = null
-    private var mCanvasBitmap: Bitmap? = null
-    private var mDrawPaint: Paint? = null
-    private var mCanvasPaint: Paint?= null
-    private var mBrushSize: Float = 0.toFloat()
+    private var mDrawPath: CustomPath? =
+        null // An variable of CustomPath inner class to use it further.
+    private var mCanvasBitmap: Bitmap? = null // An instance of the Bitmap.
+    private var mDrawPaint: Paint? =
+        null // The Paint class holds the style and color information about how to draw geometries, text and bitmaps.
+    private var mCanvasPaint: Paint? = null // Instance of canvas paint view.
+    private var mBrushSize: Float =
+        0.toFloat() // A variable for stroke/brush size to draw on the canvas.
+
+    // A variable to hold a color of the stroke.
     private var color = Color.BLACK
-    private var canvas: Canvas?= null
+
+    /**
+     * A variable for canvas which will be initialized later and used.
+     *
+     *The Canvas class holds the "draw" calls. To draw something, you need 4 basic components: A Bitmap to hold the pixels, a Canvas to host
+     * the draw calls (writing into the bitmap), a drawing primitive (e.g. Rect,
+     * Path, text, Bitmap), and a paint (to describe the colors and styles for the
+     * drawing)
+     */
+
+    private var canvas: Canvas? = null
     private var mPaths = ArrayList<CustomPath>()
 
     init {
         setUpDrawing()
     }
 
-    private fun setUpDrawing(){
+    /**
+     * This method initializes the attributes of the
+     * ViewForDrawing class.
+     */
+
+    private fun setUpDrawing() {
         mDrawPaint = Paint()
-        mDrawPath = CustomPath(color,mBrushSize)
+        mDrawPath = CustomPath(color, mBrushSize)
         mDrawPaint!!.color = color
-        mDrawPaint!!.style = Paint.Style.STROKE
-        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
-        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
-        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mDrawPaint!!.style = Paint.Style.STROKE // This is to draw a STROKE style
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND // This is for store join
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND // This is for stroke Cap
+        mCanvasPaint = Paint(Paint.DITHER_FLAG) // Paint flag that enables dithering when blitting.
         mBrushSize = 20.toFloat()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        mCanvasBitmap = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
         canvas = Canvas(mCanvasBitmap!!)
     }
 
+    /**
+     * This method is called when a stroke is drawn on the canvas
+     * as a part of the painting.
+     */
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawBitmap(mCanvasBitmap!!,0f,0f,mCanvasPaint)
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+        /**
+         * Draw the specified bitmap, with its top/left corner at (x,y), using the specified paint,
+         * transformed by the current matrix.
+         *
+         *If the bitmap and canvas have different densities, this function will take care of
+         * automatically scaling the bitmap to draw at the same density as the canvas.
+         *
+         * @param bitmap The bitmap to be drawn
+         * @param left The position of the left side of the bitmap being drawn
+         * @param top The position of the top side of the bitmap being drawn
+         * @param paint The paint used to draw the bitmap (may be null)
+         */
 
-        for (path in mPaths){
+        for (path in mPaths) {
             mDrawPaint!!.strokeWidth = path.brushThickness
             mDrawPaint!!.color = path.color
-            canvas.drawPath(path,mDrawPaint!!)
-
+            canvas.drawPath(path, mDrawPaint!!)
         }
 
-        if (!mDrawPath!!.isEmpty){
+        if (!mDrawPath!!.isEmpty) {
             mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
             mDrawPaint!!.color = mDrawPath!!.color
-            canvas.drawPath(mDrawPath!!,mDrawPaint!!)
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
         }
     }
-
+    /**
+     * This method acts as an event listener when a touch
+     * event is detected on the device.
+     */
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         val touchX = event?.x
         val touchY = event?.y
-        when(event?.action){
-            MotionEvent.ACTION_DOWN ->{
-                mDrawPath!!.color=color
-                mDrawPath!!.brushThickness=mBrushSize
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
 
                 mDrawPath!!.reset()
                 if (touchX != null) {
                     if (touchY != null) {
-                        mDrawPath!!.moveTo(touchX,touchY)
+                        mDrawPath!!.moveTo(touchX, touchY)
                     }
                 }
             }
-            MotionEvent.ACTION_MOVE ->{
+
+            MotionEvent.ACTION_MOVE -> {
                 if (touchX != null) {
                     if (touchY != null) {
-                        mDrawPath!!.lineTo(touchX,touchY)
+                        mDrawPath!!.lineTo(touchX, touchY)
                     }
                 }
             }
-            MotionEvent.ACTION_UP ->{
+
+            MotionEvent.ACTION_UP -> {
                 mPaths.add(mDrawPath!!)
-                mDrawPath = CustomPath(color,mBrushSize)
+                mDrawPath = CustomPath(color, mBrushSize)
             }
-            else -> return  false
+
+            else -> return false
         }
         invalidate()
-
         return true
     }
 
-    internal inner class CustomPath(var color:Int, var brushThickness: Float) : Path() {
+    fun setSizeForBrush(newSize : Float){
+        mBrushSize = TypedValue
+    }
+
+    //An inner class for custom path with two params as color and stroke size.
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
 
     }
 }
